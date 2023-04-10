ec1da8eaf3cb1d677c85dc3fdb454c516d757ef3
Ayush Bhauwala <ayushbhauwala07@gmail.com>
2023-04-10 09:44:41 +0530

    9.2

--- Lab 9/2.cpp
+++ Lab 9/2.cpp
@@ -18,8 +18,46 @@ int main()
         queries[i][0] = left;
         queries[i][1] = right;
     }
+
+    // printing queries
+    // cout << "queries:\n";
+    // for (int i = 0; i < q; i++)
+    // {
+    //     cout << queries[i][0] << " " << queries[i][1];
+    // }
+    // cout << "\n";
+    int platesToLeft[n];
+    int platesToRight[n];
+    int count = 0;
+    int total = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '|')
+            count = total;
+        else
+            total++;
+        platesToLeft[i] = count;
+    }
+
+    count = 0;
+    total = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (s[i] == '|')
+            count = total;
+        else
+            total++;
+        platesToRight[i] = count;
+    }
+    cout << "\n---\n";
     for (int i = 0; i < q; i++)
     {
+        int l = queries[i][0];
+        int r = queries[i][1];
+        int ans = platesToRight[l] + platesToLeft[r] - total;
+        if (ans < 0)
+            ans = 0;
+        cout << ans << "\n";
     }
     return 0;
 }
