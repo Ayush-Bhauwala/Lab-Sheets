43a443777ebb68d928adcc998d5b4d78d48638b9
Ayush Bhauwala <ayushbhauwala07@gmail.com>
2023-04-16 17:09:19 +0530

    10.3

--- Lab 10/3.cpp
+++ Lab 10/3.cpp
@@ -2,16 +2,35 @@
 
 using namespace std;
 
-float findMedian(int a[], int low, int high)
+float median(int nums1[], int nums2[], int m, int n)
 {
-    if ((high - low) % 2)
+    if (m > n)
+        return median(nums2, nums1, n, m); // ensuring that binary search happens on minimum size array
+
+    int low = 0, high = m, medianPos = ((m + n) + 1) / 2;
+    while (low <= high)
     {
-        return (a[low + ((high - low) / 2)] + a[(low + ((high - low) / 2)) + 1]) / 2;
+        int cut1 = (low + high) / 2;
+        int cut2 = medianPos - cut1;
+
+        int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+        int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+        int r1 = (cut1 == m) ? INT_MAX : nums1[cut1];
+        int r2 = (cut2 == n) ? INT_MAX : nums2[cut2];
+
+        if (l1 <= r2 && l2 <= r1)
+        {
+            if ((m + n) % 2 != 0)
+                return max(l1, l2);
+            else
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
+        }
+        else if (l1 > r2)
+            high = cut1 - 1;
+        else
+            low = cut1 + 1;
     }
-    else
-    {
-        return a[low + ((high - low) / 2)];
-    }
+    return 0.0;
 }
 
 int main()
@@ -32,11 +51,7 @@ int main()
         cin >> b[i];
     }
 
-    int low_a = 0;
-    int high_a = n - 1;
-
-    int low_b = 0;
-    int high_b = m - 1;
+    cout << median(a, b, n, m);
 
     return 0;
 }
--- Lab 10/4.cpp
+++ Lab 10/4.cpp
@@ -1,15 +1,71 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
+
+int findMin(int arr[], int start, int end)
+{
+    int min = 0;
+    for (int i = start; i <= end; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+    }
+    return min;
+}
+
+void reverseArray(int arr[], int start, int end)
+{
+    int mid = (start + end) / 2;
+    for (int i = start; i <= mid; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[end - i];
+        arr[end - i] = temp;
+    }
+    // cout << "\n";
+    // for (int i = 0; i <= end; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+    // cout << "\n";
+}
 
 int main()
 {
     int n;
     cin >> n;
     int arr[n];
+    unordered_map<int, int> mp;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
+        mp[arr[i]] = i;
+    }
+    int k = 0;
+    vector<int> ks;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int num = i + 1;
+        if (arr[num - 1] != num)
+        {
+            if (index != 0)
+            {
+                reverseArray(arr, 0, index);
+                ks.push_back(index + 1);
+            }
+            if (num != 1)
+            {
+                reverseArray(arr, 0, num - 1);
+                ks.push_back(num);
+            }
+        }
+    }
+    cout << ks.size() << "\n";
+    for (int i = 0; i < ks.size(); i++)
+    {
+        cout << ks[i] << " ";
     }
     return 0;
 }
